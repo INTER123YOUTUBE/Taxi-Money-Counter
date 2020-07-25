c046faac92d4ae2911f29d60f314c2f8d93ee62d
INTER123YOUTUBE <56187364+INTER123YOUTUBE@users.noreply.github.com>
2020-07-25 09:19:38 +0900

    Add files via upload

---
+++ 택시미터기 v1.2.1 실행 런쳐-mac.bash
@@ -0,0 +1,6 @@
+cd Resources
+echo "Taxi Money Counter Version 1.2.1 [2020 Edition, Build 1203]"
+echo "---------------------------------------------------------"
+echo "현재 맥 버전은 한국어만 지원합니다."
+echo "It is now available for Korean Language in Mac Version."
+java -jar Korean.jar
